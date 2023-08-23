a1cc6eb01caea6f01273e2617f4bdfc0e8bd9487
victor <vgsa@hotmail.com>
2023-08-23 15:51:28 -0300

    Start Projetct

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default props =>
+    <Main icon="home" title="Home"
+        subtitle="Eu sou homem macho, muito hoomem.">
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema para exempleficar a construcao de um cadasto em React Js</p>
+    </Main>
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props=>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> <span style={{color: '#6610f2'}}>O</span>miny</strong>
+        </span>    
+    </footer>
